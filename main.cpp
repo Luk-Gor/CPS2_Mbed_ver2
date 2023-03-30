a46708d1210268b263fea5b377f4f844cf05a7ac
Luk-Gor <lgorecki275@gmail.com>
2023-03-30 12:51:19 +0000

    First

---
+++ main.cpp
@@ -0,0 +1,33 @@
+//Uses the measured z-acceleration to drive leds 2 and 3 of the mbed
+
+#include "mbed.h"
+#include "MMA7660.h"
+#include <cmath>
+#define PI 3.141592654;
+
+MMA7660 MMA(p28, p27);
+
+DigitalOut connectionLed(LED1);
+PwmOut Zaxis_p(LED2);
+PwmOut Zaxis_n(LED3);
+
+float calculateAngle(float x, float y, float z){
+    float angle = 0;
+    float bottom_part = sqrt((y*y)+(z*z));
+    float angle_rad = atan(x/bottom_part);
+    angle = (angle_rad * 180)/PI;
+    return angle;   
+
+}
+
+int main() {  
+    if (MMA.testConnection())
+        connectionLed = 1;
+
+    while(1){
+
+        wait(1);
+        printf("result %f \r\n", calculateAngle(MMA.x(), MMA.y(), MMA.z()));
+    }
+        
+}
